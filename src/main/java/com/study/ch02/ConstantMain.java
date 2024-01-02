0bd8732c72524a85fb205184f7bb94db74c878ef
GyunNote <rlaehrbs1968@naver.com>
2024-01-02 16:14:17 +0900

    init : 수업자료최초커밋

---
+++ src/main/java/com/study/ch02/ConstantMain.java
@@ -0,0 +1,13 @@
+package com.study.ch02;
+
+public class ConstantMain {
+    public static void main(String[] args) {
+
+        //상수는 대문자로만 표기한다
+        // 최초한번만 대입가능
+        //무조건 초기화가 되어야한다.
+        final String FILE_PATH = "c:/AWS/project";
+        //path = "d:/AWS/project";
+        //final 을 앞에 붙이면 주소 변경 불가능
+    }
+}
